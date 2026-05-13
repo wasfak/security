441f5d2a23121f0124b7ffaac527659efea1a3d0
wasfak <hardmodz@yahoo.com>
2026-05-13 08:34:18 +0300

    finished

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const jetbrainsMono = JetBrains_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Secrt CRM",
+  title: "Supply Chain",
   description: "Company purchase contact manager",
 };
 
